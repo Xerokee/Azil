65cadf494c93beb7c6b85a0f412ecf8cc1c0e6bf
Matija M <margeta.matija@gmail.com>
2023-12-29 00:24:26 +0100

    Structure of Navigation for Animals

---
+++ app/src/main/java/com/activity/vuv_azil_navigation/ui/category/CategoryFragment.java
@@ -0,0 +1,68 @@
+package com.activity.vuv_azil_navigation.ui.category;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.activity.vuv_azil_navigation.R;
+import com.activity.vuv_azil_navigation.adapters.NavCategoryAdapter;
+import com.activity.vuv_azil_navigation.adapters.PopularAdapters;
+import com.activity.vuv_azil_navigation.models.NavCategoryModel;
+import com.activity.vuv_azil_navigation.models.PopularModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryFragment extends Fragment {
+
+    FirebaseFirestore db;
+    RecyclerView recyclerView;
+    List<NavCategoryModel> categoryModelList;
+    NavCategoryAdapter navCategoryAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_category, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        recyclerView = root.findViewById(R.id.cat_rec);
+
+        // Popularne životinje
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL, false));
+        categoryModelList = new ArrayList<>();
+        navCategoryAdapter = new NavCategoryAdapter(getActivity(),categoryModelList);
+        recyclerView.setAdapter(navCategoryAdapter);
+
+        db.collection("NavCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                NavCategoryModel navCategoryModel = document.toObject(NavCategoryModel.class);
+                                categoryModelList.add(navCategoryModel);
+                                navCategoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/activity/vuv_azil_navigation/ui/home/HomeFragment.java
@@ -0,0 +1,144 @@
+package com.activity.vuv_azil_navigation.ui.home;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.activity.vuv_azil_navigation.R;
+import com.activity.vuv_azil_navigation.adapters.HomeAdapter;
+import com.activity.vuv_azil_navigation.adapters.PopularAdapters;
+import com.activity.vuv_azil_navigation.adapters.RecommendedAdapter;
+import com.activity.vuv_azil_navigation.databinding.FragmentHomeBinding;
+import com.activity.vuv_azil_navigation.models.HomeCategory;
+import com.activity.vuv_azil_navigation.models.PopularModel;
+import com.activity.vuv_azil_navigation.models.RecommendedModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    ScrollView scrollView;
+    ProgressBar progressBar;
+    RecyclerView popularRec,homeCatRec,recommendedRec;
+    FirebaseFirestore db;
+
+    // Popularne Životinje
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
+
+    // Početna Kategorija
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
+
+    // Predložene Životinje
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        db = FirebaseFirestore.getInstance();
+
+        popularRec = root.findViewById(R.id.pop_rec);
+        homeCatRec = root.findViewById(R.id.explore_rec_);
+        recommendedRec = root.findViewById(R.id.recommended_rec);
+        scrollView = root.findViewById(R.id.scroll_view);
+        progressBar= root.findViewById(R.id.progressbar);
+
+        progressBar.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+
+        // Popularne životinje
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false));
+        popularModelList = new ArrayList<>();
+        popularAdapters = new PopularAdapters(getActivity(),popularModelList);
+        popularRec.setAdapter(popularAdapters);
+
+        db.collection("PopularAnimals")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel = document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapters.notifyDataSetChanged();
+
+                                progressBar.setVisibility(View.GONE);
+                                scrollView.setVisibility(View.VISIBLE);
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        // Početna Kategorija
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false));
+        categoryList = new ArrayList<>();
+        homeAdapter = new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        db.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                HomeCategory homeCategory = document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        // Predložene Životinje
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL, false));
+        recommendedModelList = new ArrayList<>();
+        recommendedAdapter = new RecommendedAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        db.collection("RecommendedAnimals")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                RecommendedModel recommendedModel = document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        return root;
+    }
+}
