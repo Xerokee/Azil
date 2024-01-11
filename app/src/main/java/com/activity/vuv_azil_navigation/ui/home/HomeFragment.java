9d63510ad37c9c2d5c50444519144368a00facda
Matija M <margeta.matija@gmail.com>
2024-01-11 15:49:02 +0100

    Search functionality for Animals

--- app/src/main/java/com/activity/vuv_azil_navigation/ui/home/HomeFragment.java
+++ app/src/main/java/com/activity/vuv_azil_navigation/ui/home/HomeFragment.java
@@ -3,10 +3,13 @@ package com.activity.vuv_azil_navigation.ui.home;
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -22,12 +25,15 @@ import com.activity.vuv_azil_navigation.R;
 import com.activity.vuv_azil_navigation.adapters.HomeAdapter;
 import com.activity.vuv_azil_navigation.adapters.PopularAdapters;
 import com.activity.vuv_azil_navigation.adapters.RecommendedAdapter;
+import com.activity.vuv_azil_navigation.adapters.ViewAllAdapter;
 import com.activity.vuv_azil_navigation.databinding.FragmentHomeBinding;
 import com.activity.vuv_azil_navigation.models.HomeCategory;
 import com.activity.vuv_azil_navigation.models.PopularModel;
 import com.activity.vuv_azil_navigation.models.RecommendedModel;
+import com.activity.vuv_azil_navigation.models.ViewAllModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -45,6 +51,12 @@ public class HomeFragment extends Fragment {
     // Popularne Životinje
     List<PopularModel> popularModelList;
     PopularAdapters popularAdapters;
+
+    // Search view
+    EditText search_box;
+    private List<ViewAllModel> viewAllModelList;
+    private RecyclerView recyclerViewSearch;
+    private ViewAllAdapter viewAllAdapter;
 
     // Početna Kategorija
     List<HomeCategory> categoryList;
@@ -139,6 +151,59 @@ public class HomeFragment extends Fragment {
                         }
                     }
                 });
+
+        // Search view
+        recyclerViewSearch = root.findViewById(R.id.search_rec);
+        search_box = root.findViewById(R.id.search_box);
+        viewAllModelList = new ArrayList<>();
+        viewAllAdapter = new ViewAllAdapter(getContext(),viewAllModelList);
+        recyclerViewSearch.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewSearch.setAdapter(viewAllAdapter);
+        recyclerViewSearch.setHasFixedSize(true);
+        search_box.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.toString().isEmpty()){
+                    viewAllModelList.clear();
+                    viewAllAdapter.notifyDataSetChanged();
+                }else {
+                    searchAnimal(s.toString());
+                }
+            }
+        });
+
         return root;
+    }
+
+    private void searchAnimal(String type) {
+
+        if (!type.isEmpty()){
+            db.collection("AllAnimals").whereEqualTo("type",type).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful() && task.getResult() != null){
+                                viewAllModelList.clear();
+                                viewAllAdapter.notifyDataSetChanged();
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ViewAllModel viewAllModel = doc.toObject(ViewAllModel.class);
+                                    viewAllModelList.add(viewAllModel);
+                                    viewAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
     }
 }
